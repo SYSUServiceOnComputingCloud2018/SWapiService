e3a24bd501179801e7fd57654b4a12a7bab12c9b
Wangyx <469410930@qq.com>
2018-12-08 18:33:47 +0800

    实现film vehicle starship plannets people handlers方法

--- boltdb/service/handlers.go
+++ boltdb/service/handlers.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/SYSUServiceOnComputingCloud2018/SWapiService/boltdb/dbOperator"
+	"github.com/SWapiService/boltdb/dbOperator"
 	"github.com/boltdb/bolt"
 	"github.com/gorilla/mux"
 	"github.com/peterhellberg/swapi"
@@ -116,7 +116,15 @@ func planetsHandler(formatter *render.Render) http.HandlerFunc {
 }
 
 func filmsHandler(formatter *render.Render) http.HandlerFunc {
-	fmt.Println("test")
+	// 输出schema
+	// jsonData, _ := dbOperator.GetSchemaByBucket(db, "Film")
+	// var schema Schema //定义在crawler.go中
+	// err = json.Unmarshal(jsonData, &schema)
+	// if err == nil {
+	// 	fmt.Println(schema)
+	// } else {
+	// 	fmt.Println(err)
+	// }
 	return func(w http.ResponseWriter, req *http.Request) {
 
 		db, err := bolt.Open("my.db", 0600, nil)
@@ -130,13 +138,55 @@ func filmsHandler(formatter *render.Render) http.HandlerFunc {
 		vars := mux.Vars(req)
 		id := vars["id"]
 
-		// 从db中获得Planet Struct
+		// 从db中获得Film Struct
 		v, err := dbOperator.GetElementById(db, "Film", id)
 		if err != nil {
 			fmt.Println(err)
 			WriteResponse(w, ErrorResponseCode, "failed", nil)
 		} else {
-			var data swapi.Planet
+			var data swapi.Film
+			err = json.Unmarshal(v, &data)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(data.Title)
+				WriteResponse(w, SuccessResponseCode, "success", data)
+			}
+		}
+	}
+}
+
+func speciesHandler(formatter *render.Render) http.HandlerFunc {
+
+	// 输出schema
+	// jsonData, _ := dbOperator.GetSchemaByBucket(db, "Species")
+	// var schema Schema //定义在crawler.go中
+	// err = json.Unmarshal(jsonData, &schema)
+	// if err == nil {
+	// 	fmt.Println(schema)
+	// } else {
+	// 	fmt.Println(err)
+	// }
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		db, err := bolt.Open("my.db", 0600, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer db.Close()
+		// fmt.Println("URL", req.URL, "HOST", req.Host, "Method", req.Method, "RequestURL", req.RequestURI, "RawQuery", req.URL.RawQuery)
+
+		// 获取id
+		vars := mux.Vars(req)
+		id := vars["id"]
+
+		// 从db中获得Film Struct
+		v, err := dbOperator.GetElementById(db, "Species", id)
+		if err != nil {
+			fmt.Println(err)
+			WriteResponse(w, ErrorResponseCode, "failed", nil)
+		} else {
+			var data swapi.Species
 			err = json.Unmarshal(v, &data)
 			if err != nil {
 				fmt.Println(err)
@@ -148,24 +198,87 @@ func filmsHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
-func speciesHandler(formatter *render.Render) http.HandlerFunc {
-
-	return func(w http.ResponseWriter, req *http.Request) {
-		//
-	}
-}
-
 func vehiclesHandler(formatter *render.Render) http.HandlerFunc {
 
+	// 输出schema
+	// jsonData, _ := dbOperator.GetSchemaByBucket(db, "Vehicle")
+	// var schema Schema //定义在crawler.go中
+	// err = json.Unmarshal(jsonData, &schema)
+	// if err == nil {
+	// 	fmt.Println(schema)
+	// } else {
+	// 	fmt.Println(err)
+	// }
 	return func(w http.ResponseWriter, req *http.Request) {
-		//
+
+		db, err := bolt.Open("my.db", 0600, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer db.Close()
+		// fmt.Println("URL", req.URL, "HOST", req.Host, "Method", req.Method, "RequestURL", req.RequestURI, "RawQuery", req.URL.RawQuery)
+
+		// 获取id
+		vars := mux.Vars(req)
+		id := vars["id"]
+
+		// 从db中获得Film Struct
+		v, err := dbOperator.GetElementById(db, "Vehicle", id)
+		if err != nil {
+			fmt.Println(err)
+			WriteResponse(w, ErrorResponseCode, "failed", nil)
+		} else {
+			var data swapi.Vehicle
+			err = json.Unmarshal(v, &data)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(data.Name)
+				WriteResponse(w, SuccessResponseCode, "success", data)
+			}
+		}
 	}
 }
 
 func starshipsHandler(formatter *render.Render) http.HandlerFunc {
 
+	// 输出schema
+	// jsonData, _ := dbOperator.GetSchemaByBucket(db, "Starship")
+	// var schema Schema //定义在crawler.go中
+	// err = json.Unmarshal(jsonData, &schema)
+	// if err == nil {
+	// 	fmt.Println(schema)
+	// } else {
+	// 	fmt.Println(err)
+	// }
 	return func(w http.ResponseWriter, req *http.Request) {
-		//
+
+		db, err := bolt.Open("my.db", 0600, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer db.Close()
+		// fmt.Println("URL", req.URL, "HOST", req.Host, "Method", req.Method, "RequestURL", req.RequestURI, "RawQuery", req.URL.RawQuery)
+
+		// 获取id
+		vars := mux.Vars(req)
+		id := vars["id"]
+
+		// 从db中获得Film Struct
+		v, err := dbOperator.GetElementById(db, "Starship", id)
+		if err != nil {
+			fmt.Println(err)
+			WriteResponse(w, ErrorResponseCode, "failed", nil)
+		} else {
+			var data swapi.Starship
+			err = json.Unmarshal(v, &data)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(data.Name)
+				WriteResponse(w, SuccessResponseCode, "success", data)
+			}
+		}
 	}
 }
 
